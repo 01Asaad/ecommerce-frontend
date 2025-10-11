8dcdfd6a3a74fe97a59f29e8b0ac0f2b18ad8072
Asaad Ali <asaad.ali.dev@gmail.com>
2025-10-11 23:01:04 +0300

    show loading bar for queries run by react-query too

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -2,14 +2,17 @@ import { Outlet, useNavigation, useFetchers } from "react-router-dom";
 import GlobalNavBar from "../components/GlobalNavBar";
 import LoadingBar from "../components/UI/LoadingBar"
 import { useUser } from "../context/UserProvider";
+import { useIsFetching } from '@tanstack/react-query';
+
 export default function RootLayout() {
     const navigation = useNavigation()
+    const queriesFetchingLen = useIsFetching()
     const userCtx = useUser()
     const fetchers = useFetchers()
     const isAnyFetcherLoading = fetchers.some(fetcher =>
         fetcher.state !== "idle"
     );
-    const isLoading = userCtx.isLoading || navigation.state !== "idle" || isAnyFetcherLoading
+    const isLoading = userCtx.isLoading || navigation.state !== "idle" || isAnyFetcherLoading || queriesFetchingLen
     return (<>
         <header className="sticky top-0 z-50">
             <GlobalNavBar />
